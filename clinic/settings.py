ef2af3cae7f7b50a8a85a5cacdbe1144814bfcda
IbnuEyni <shuaibahmedin@gmail.com>
2024-07-11 21:53:44 +0300

    config

--- clinic/settings.py
+++ clinic/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
